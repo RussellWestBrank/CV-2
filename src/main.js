1d467ff467075faafe77fae5b0a220e870ad3324
Russ <851911336@qq.com>
2020-09-05 23:53:50 +0800

    修改字体大小

--- src/main.js
+++ src/main.js
@@ -1,18 +1,1 @@
-function getSubArr(arr, min, max) {
-  let a = 0;
-  let b = 0;
-  for (var i of arr) {
-    if (min <= i && i <= max) {
-      a++;
-    } else {
-      for (var j = 1; j <= a; j++) {
-        b += j;
-      }
-      a = 0;
-    }
-  }
-  for (var j = 1; j <= a; j++) {
-    b += j;
-  }
-  return b;
-}
+
